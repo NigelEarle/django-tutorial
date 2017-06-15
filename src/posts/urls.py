2101700e530dec930fa01f86dc045ace5d8feddd
nwre87 <nwre87@gmail.com>
2017-06-15 12:51:21 -1000

    add crud urls to posts app with subrouting from src

---
+++ src/posts/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.post_list),
+    url(r'^create/$', views.post_create),
+    url(r'^detail/$', views.post_detail),
+    url(r'^update/$', views.post_update),
+    url(r'^delete/$', views.post_delete),
+]
--- src/posts/views.py
+++ src/posts/views.py
@@ -3,5 +3,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def posts_home(request):
-  return HttpResponse("<h1>Hello</h1>")
+def post_create(request):
+  return HttpResponse("<h1>Create</h1>")
+
+def post_detail(request):
+  return HttpResponse("<h1>Detail</h1>")
+
+def post_list(request):
+  return HttpResponse("<h1>List</h1>")
+
+def post_update(request):
+  return HttpResponse("<h1>Update</h1>")
+
+def post_delete(request):
+  return HttpResponse("<h1>Delete</h1>")
+
