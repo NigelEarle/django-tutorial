712045da2a42ac5629f9bf8228b942873a5da0ad
nwre87 <nwre87@gmail.com>
2017-06-15 12:07:37 -1000

    add admin customization and change post to content

--- src/posts/models.py
+++ src/posts/models.py
@@ -6,10 +6,10 @@ from django.db import models
 
 class Post(models.Model):
   title = models.CharField(max_length=120)
-  post = models.TextField()
+  content = models.TextField()
   updated = models.DateTimeField(auto_now=True, auto_now_add=False)
   timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
 
-  def __unicode__(self):
+  def __unicode__(self): # shows title in admin console
     return self.title
