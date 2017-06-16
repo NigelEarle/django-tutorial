09a9a29094f968ef9678c488e66a9a96f81b75de
nwre87 <nwre87@gmail.com>
2017-06-15 15:40:56 -1000

    add PostForm class with html form

--- src/posts/views.py
+++ src/posts/views.py
@@ -2,10 +2,27 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .forms import PostForm
 from .models import Post
 
 def post_create(request):
-  return HttpResponse("<h1>Create</h1>")
+  form = PostForm(request.POST or None)
+
+  if form.is_valid():
+    instance = form.save(commit=False)
+    print form.cleaned_data.get("title")
+    instance.save()
+
+  # if request.method == "POST":
+  #   print request.POST.get("title")
+  #   print request.POST.get("content")
+
+
+  context = {
+    "form" : form,
+  }
+  return render(request, "post_form.html", context);
+  # return HttpResponse("<h1>Create</h1>")
 
 def post_detail(request, id=None):
   instance = get_object_or_404(Post, id=id)
