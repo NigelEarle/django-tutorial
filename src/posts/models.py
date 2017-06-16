c75388f955ce5c8ed72508884367ca4c8add26f1
nwre87 <nwre87@gmail.com>
2017-06-15 22:13:36 -1000

    add ImageField and slug

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,7 +1,9 @@
 from __future__ import unicode_literals
-from django.db import models
 from django.core.urlresolvers import reverse
+from django.db import models
+from django.db.models.signals import pre_save
 
+from django.utils.text import slugify
 # Create your models here.
 
 def upload_location(instance, filename):
@@ -9,6 +11,7 @@ def upload_location(instance, filename):
 
 class Post(models.Model):
   title = models.CharField(max_length=120)
+  slug = models.SlugField(unique=True)
   image = models.ImageField(upload_to=upload_location, null=True, blank=True, width_field="width_field", height_field="height_field")
   height_field = models.IntegerField(default=0)
   width_field = models.IntegerField(default=0)
@@ -25,3 +28,21 @@ class Post(models.Model):
 
   class Meta:
     ordering = ["-timestamp", "-updated"]
+
+
+def create_slug(instance, new_slug=None):
+  slug = slugify(instance.title)
+  if new_slug is not None:
+    slug = new_slug
+  qs = Post.objects.filter(slug=slug).order_by("-id")
+  exists = qs.exists()
+  if exists:
+    new_slug = "%s-%s" %(slug, qs.first().id)
+    return create_slug(instance, new_slug=new_slug)
+  return slug
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+  if not instance.slug:
+    instance.slug = create_slug(instance)
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
