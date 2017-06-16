6c86ecb6b18346427a7ddbc6cec42bce93837ee1
nwre87 <nwre87@gmail.com>
2017-06-15 22:30:34 -1000

    add social links and quoteplus share_link

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,5 @@
+from urllib import quote_plus
+
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404, redirect
@@ -23,9 +25,11 @@ def post_create(request):
 
 def post_detail(request, id=None):
   instance = get_object_or_404(Post, id=id)
+  share_string = quote_plus(instance.content)
   context = {
     "instance": instance,
-    "title" : "Detail"
+    "title" : "Detail",
+    "share_string": share_string
   }
   return render(request, "post_detail.html", context)
 
