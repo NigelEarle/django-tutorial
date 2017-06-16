14f89bc10a1a3fec1d1dbe3272d0768c53b7e50c
nwre87 <nwre87@gmail.com>
2017-06-15 19:39:31 -1000

    add pagination

--- src/posts/models.py
+++ src/posts/models.py
@@ -16,3 +16,6 @@ class Post(models.Model):
   def get_absolute_url(self):
     return reverse("posts:detail", kwargs={"id": self.id})
     # return 'posts/%s/' %(self.id)
+
+  class Meta:
+    ordering = ["-timestamp", "-updated"]
