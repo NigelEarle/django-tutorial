75ac3e62d213a00a8fbafc4b3a32e64ca9732492
nwre87 <nwre87@gmail.com>
2017-06-15 14:44:39 -1000

    add post_detail.html

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import Post
@@ -8,10 +8,12 @@ def post_create(request):
   return HttpResponse("<h1>Create</h1>")
 
 def post_detail(request):
+  instance = get_object_or_404(Post, id=3)
   context = {
+    "instance": instance,
     "title" : "Detail"
   }
-  return render(request, "index.html", context)
+  return render(request, "post_detail.html", context)
   # return HttpResponse("<h1>Detail</h1>")
 
 def post_list(request):
